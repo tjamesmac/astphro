9cd818aa4d711259e7a7278fcbe98eac3e692839
Thomas McAuliffe <tmcauliffe@roku.com>
2023-09-06 17:04:57 +0100

    fix: env vars

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
+// const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export const getPosts = async () =>
